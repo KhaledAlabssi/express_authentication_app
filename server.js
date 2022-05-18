9e94a31bbd3a62376cdeb249be8132a0b172c39d
KhaledAlabssi <khaled.absi5@gmail.com>
2022-05-18 16:14:22 +0200

    create user routers/controllers for register and login

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('server...');
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT || 5000
+
+import userRoutes from './routes/userRoutes.js'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to Froex App'})
+})
+
+app.use('/api/users', userRoutes)
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
